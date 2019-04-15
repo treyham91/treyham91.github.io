54fd36fa9f96891f6182a484f081284e0f4ecd09
Trey <trey.hamilton21@gmail.com>
2019-04-14 22:19:25 -0500

    initial commit

---
+++ JavaScript/Home.js
@@ -0,0 +1,33 @@
+document.addEventListener('scroll', function () {
+    var about = document.getElementById("about-container");
+    var projects = document.getElementById("projects-container");
+    var skills = document.getElementById("skills-container");
+
+    var aboutBody = about.getBoundingClientRect();
+    var projectsBody = projects.getBoundingClientRect();
+    var skillsBody = skills.getBoundingClientRect();
+
+    if (window.scrollY > aboutBody.top) {
+        about.style.visibility = 'visible';
+        about.style.animation = 'fadein 3s';
+    }
+
+    if (window.scrollY > projectsBody.top) {
+        projects.style.visibility = 'visible';
+        projects.style.animation = 'fadein 3s';
+    }
+
+    if (window.scrollY > skillsBody.top) {
+        skills.style.visibility = 'visible';
+        skills.style.animation = 'fadein 3s';
+    }
+})
+
+document.getElementById("side-nav-bar").addEventListener("click", function () {
+    this.classList.toggle("open");
+    document.getElementById("mySidenav").style.width = "250px";
+})
+
+document.getElementById("closebtn").addEventListener("click", function () {
+    this.style.width = "0";
+})
