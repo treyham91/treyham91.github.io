a17be8996c758a26f15aa3b4a2b3f30a50be2085
Trey <trey.hamilton21@gmail.com>
2019-05-07 01:24:56 -0500

    global layout file for side nav events

--- JavaScript/Home.js
+++ JavaScript/Home.js
@@ -22,11 +22,3 @@ document.addEventListener('scroll', function () {
         skills.style.animation = 'fadein 3s';
     }
 })
-
-document.getElementById("side-nav-bar").addEventListener("click", function () {
-    document.getElementById("mySidenav").style.width = "250px";
-})
-
-document.getElementById("closebtn").addEventListener("click", function () {
-    document.getElementById("mySidenav").style.width = "0";
-})
---
+++ JavaScript/Layout.js
@@ -0,0 +1,7 @@
+document.getElementById("side-nav-bar").addEventListener("click", function () {
+    document.getElementById("mySidenav").style.width = "250px";
+});
+
+document.getElementById("closebtn").addEventListener("click", function () {
+    document.getElementById("mySidenav").style.width = "0";
+});
