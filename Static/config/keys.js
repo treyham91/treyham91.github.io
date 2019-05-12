e824a93bfa284218d9b28993d116177345f0413c
Trey <trey.hamilton21@gmail.com>
2019-05-12 02:48:17 -0500

    home page

---
+++ Static/config/keys.js
@@ -0,0 +1,20 @@
+module.exports = {
+
+    emailConfig = {
+
+        emailKeys = {
+            username: 'trey.hamilton21',
+            password: 'Rosherun2001!@#'
+        },
+
+        email = {
+            email: 'trey.hamilton21@gmail.com'
+        },
+
+        emailService = {
+            userId: 'user_mlIJr0OHx7aFpvjOuYlvF',
+            serviceId: 'trey_hamilton21_gmail_com',
+            templateId: 'template_ZYdWKszn'
+        }
+    }
+}
