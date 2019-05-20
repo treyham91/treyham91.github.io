a5aec70880b40194e9cff38fe35512e55bbeded1
Trey Hamilton <32377378+treyham91@users.noreply.github.com>
2019-05-20 00:35:15 -0500

    Second commit (#7)
    * home page
    
    * home page
    
    * project description
    
    * image update
    
    * side nav updates

--- JavaScript/Layout.js
+++ JavaScript/Layout.js
@@ -5,3 +5,9 @@ document.getElementById("side-nav-bar").addEventListener("click", function () {
 document.getElementById("closebtn").addEventListener("click", function () {
     document.getElementById("mySidenav").style.width = "0";
 });
+
+window.addEventListener("resize", function () {
+    if (window.innerWidth > 670) {
+        document.getElementById("mySidenav").style.width = "0";
+    }
+})
